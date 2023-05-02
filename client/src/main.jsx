64fc069b7331fd6f86d9f1b49ce4d5d892708854
VuTrung <vutrung26072001@gmail.com>
2023-05-02 13:38:29 +0700

    Test toggle theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Theme } from "react-daisyui";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Theme dataTheme="dark">
+      <App />
+    </Theme>
   </React.StrictMode>
 );
