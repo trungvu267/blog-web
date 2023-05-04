c7708d515cee980e36616004909b163365e78687
VuTrung <vutrung26072001@gmail.com>
2023-05-05 01:33:35 +0700

    add page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,39 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Theme } from "react-daisyui";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomePage, NotFoundPage } from "./pages";
+import {
+  HomePage,
+  NotFoundPage,
+  LoginPage,
+  ViewPost,
+  CreatePost,
+  Setting,
+  SignUpPage,
+} from "./pages";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/posts/:postId",
+    element: <ViewPost />,
+  },
+  {
+    path: "/posts/create",
+    element: <CreatePost />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/setting",
+    element: <Setting />,
   },
   {
     path: "*",
