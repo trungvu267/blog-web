214634cf8e37ed94fefac4a80652640139cbd400
VuTrung <vutrung26072001@gmail.com>
2023-05-05 01:22:36 +0700

    cofig routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Theme } from "react-daisyui";
-import App from "./App.jsx";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage, NotFoundPage } from "./pages";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Theme dataTheme="dark">
-      <App />
+      <RouterProvider router={router} />
     </Theme>
   </React.StrictMode>
 );
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Button } from "react-daisyui";
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default HomePage;
