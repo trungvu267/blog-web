53803aebe21c2556c2431d6c88c366ac37f5879d
VuTrung <vutrung26072001@gmail.com>
2023-05-19 02:22:43 +0700

    sửa lỗi chính tả

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -1,142 +1,175 @@
 import Blog from "../models/blog.model.js";
 import { StatusCodes } from "http-status-codes";
 
-const getAllBlog = async(req, res) => {
-    try {
-        const blog = await Blog.find({ author: req.user.userId, }).populate('author', [
-            'name'
-        ])
-        res.json({ success: true, blog })
-    } catch (error) {
-        console.log(error)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: 'Internal server error' })
+const getAllBlog = async (req, res) => {
+  try {
+    const blog = await Blog.find({ author: req.user.userId }).populate(
+      "author",
+      ["name"]
+    );
+    res.json({ success: true, blog });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+const getOne = async (req, res) => {
+  try {
+    const blogCondition = { _id: req.params.id, author: req.user.userId };
+    const blog = await Blog.findOne(blogCondition);
+
+    if (!blog) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ success: false, message: "post not found" });
     }
-}
 
-const getOne = async(req, res) => {
-    try {
-        const blogCondition = { _id: req.params.id, author: req.user.userId }
-        const blog = await Blog.findOne(blogCondition)
-
-        if (!blog) {
-            return res.status(StatusCodes.NOT_FOUND).json({ success: false, message: 'post not found' })
-        }
-
-        res.json({ success: true, message: 'fin post success', blog: blog })
-    } catch (error) {
-        console.log(error)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: 'Internal server error' })
-    }
-
-}
-
+    res.json({ success: true, message: "fin post success", blog: blog });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
 
 // Create a new blog
-const createBlog = async(req, res) => {
-    const { title, content } = req.body
+const createBlog = async (req, res) => {
+  const { title, content } = req.body;
 
-    if (!title) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ success: false, message: 'you must be provide title' })
+  if (!title) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: "you must be provide title" });
+  }
+  if (!(content.length > 6)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: "you must be provide content" });
+  }
+
+  if (!(req.user.role === "administrator")) {
+    return res
+      .status(StatusCodes.FORBIDDEN)
+      .json({ success: false, message: "You do not have permission to edit" });
+  }
+
+  try {
+    const newBlog = new Blog({
+      title,
+      content,
+      author: req.user.userId,
+    });
+
+    await newBlog.save();
+
+    res.json({
+      success: true,
+      message: "you have successfully created",
+      blog: newBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+const updateBlog = async (req, res) => {
+  const { title, content, published } = req.body;
+
+  // Simple validation
+  if (!title) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: "you must be provide title" });
+  }
+  if (!(content.length > 6)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: "you must be provide content" });
+  }
+  if (!(req.user.role === "administrator")) {
+    return res
+      .status(StatusCodes.FORBIDDEN)
+      .json({ success: false, message: "You do not have permission to edit" });
+  }
+
+  try {
+    let updatedBlog = {
+      title,
+      content,
+      published: published ? published : false,
+    };
+
+    const blogUpdateCondition = { _id: req.params.id, author: req.user.userId };
+
+    const blog = await Blog.findOne(blogUpdateCondition);
+    if (!blog) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ success: false, message: "post not found" });
     }
-    if (!(content.length > 6)) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ success: false, message: 'you must be provide content' })
+
+    const newUpdatedBlog = await Blog.findOneAndUpdate(
+      blogUpdateCondition,
+      updatedBlog,
+      { new: true }
+    );
+
+    // User not authorised to update post or post not found
+    if (!newUpdatedBlog)
+      return res.status(StatusCodes.FORBIDDEN).json({
+        success: false,
+        message: "Post not found or user not authorised",
+      });
+
+    res.json({
+      success: true,
+      message: "updated post was successfully updated",
+      post: newUpdatedBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+const deleteBlog = async (req, res) => {
+  try {
+    const blogDeleteCondition = { _id: req.params.id, author: req.user.userId };
+    const blog = await Blog.findOne(blogDeleteCondition);
+
+    if (!blog) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ success: false, message: "post not found" });
     }
 
-    if (!(req.user.role === "administrator")) {
-        return res.status(StatusCodes.FORBIDDEN).json({ success: false, message: 'You do not have permission to edit' })
-    }
+    const deletedBlog = await Blog.findOneAndDelete(blogDeleteCondition);
 
+    // User not authorised or post not found
+    if (!deletedBlog)
+      return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: "Post not found or user not authorised",
+      });
 
-    try {
-        const newBlog = new Blog({
-            title,
-            content,
-            author: req.user.userId,
+    res.json({
+      success: true,
+      message: "delete post success",
+      blog: deletedBlog,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(INTERNAL_SERVER_ERROR)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
 
-        })
-
-        await newBlog.save()
-
-        res.json({ success: true, message: 'you have successfully created', blog: newBlog })
-    } catch (error) {
-        console.log(error)
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ success: false, message: 'Internal server error' })
-    }
-}
-const updateBlog = async(req, res) => {
-    const { title, content, published } = req.body
-
-    // Simple validation
-    if (!title) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ success: false, message: 'you must be provide title' })
-    }
-    if (!(content.length > 6)) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ success: false, message: 'you must be provide content' })
-    }
-    if (!(req.user.role === "administrator")) {
-        return res.status(StatusCodes.FORBIDDEN).json({ success: false, message: 'You do not have permission to edit' })
-    }
-
-
-    try {
-        let updatedBlog = {
-            title,
-            content,
-            published: published ? published : false
-        }
-
-        const blogUpdateCondition = { _id: req.params.id, author: req.user.userId }
-
-        const blog = await Blog.findOne(blogUpdateCondition)
-        if (!blog) {
-            return res.status(StatusCodes.NOT_FOUND).json({ success: false, message: 'post not found' })
-        }
-
-        const newupdatedBlog = await Blog.findOneAndUpdate(
-            blogUpdateCondition,
-            updatedBlog, { new: true }
-        )
-
-        // User not authorised to update post or post not found
-        if (!newupdatedBlog)
-            return res.status(StatusCodes.FORBIDDEN).json({
-                success: false,
-                message: 'Post not found or user not authorised'
-            })
-
-        res.json({
-            success: true,
-            message: 'updated post was successfully updated',
-            post: newupdatedBlog
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(INTERNAL_SERVER_ERROR).json({ success: false, message: 'Internal server error' })
-    }
-}
-const deleteBolg = async(req, res) => {
-    try {
-        const blogDeleteCondition = { _id: req.params.id, author: req.user.userId }
-        const blog = await Blog.findOne(blogDeleteCondition)
-
-        if (!blog) {
-            return res.status(StatusCodes.NOT_FOUND).json({ success: false, message: 'post not found' })
-        }
-
-        const deletedBlog = await Blog.findOneAndDelete(blogDeleteCondition)
-
-        // User not authorised or post not found
-        if (!deletedBlog)
-            return res.status(StatusCodes.NOT_FOUND).json({
-                success: false,
-                message: 'Post not found or user not authorised'
-            })
-
-        res.json({ success: true, message: 'delete post success', blog: deletedBlog })
-    } catch (error) {
-        console.log(error)
-        res.status(INTERNAL_SERVER_ERROR).json({ success: false, message: 'Internal server error' })
-    }
-}
-
-export { getAllBlog, getOne, createBlog, updateBlog, deleteBolg }
+export { getAllBlog, getOne, createBlog, updateBlog, deleteBlog };
--- server/routes/blogs.route.js
+++ server/routes/blogs.route.js
@@ -1,33 +1,44 @@
 import express from "express";
 import passport from "passport";
 
-import { getAllBlog, getOne, createBlog, updateBlog, deleteBolg } from "../controllers/blog.controller.js";
+import {
+  getAllBlog,
+  getOne,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+} from "../controllers/blog.controller.js";
 
 const router = express.Router();
 
-// @route GET /blogs/getall
-// @desc Get blogs
-// @access Private
-router.get('/getall', passport.authenticate("bearer", { session: false }), getAllBlog)
+router.get(
+  "/getall",
+  passport.authenticate("bearer", { session: false }),
+  getAllBlog
+);
 
-// @route GET /blogs/getone/:id
-// @desc Get blogs
-// @access Private
-router.get('/getone/:id', passport.authenticate("bearer", { session: false }), getOne)
+router.get(
+  "/getone/:id",
+  passport.authenticate("bearer", { session: false }),
+  getOne
+);
 
-// @route Put /blogs/update
-// @desc Put blogs
-// @access Private
-router.put('/update/:id', passport.authenticate("bearer", { session: false }), updateBlog)
+router.put(
+  "/update/:id",
+  passport.authenticate("bearer", { session: false }),
+  updateBlog
+);
 
-// @route POST /blogs/create
-// @desc Create blogs
-// @access Private
-router.post('/create', passport.authenticate("bearer", { session: false }), createBlog)
+router.post(
+  "/create",
+  passport.authenticate("bearer", { session: false }),
+  createBlog
+);
 
-// @route /delete/:id
-// @desc delete
-// @access Private
-router.delete('/delete/:id', passport.authenticate("bearer", { session: false }), deleteBolg)
+router.delete(
+  "/delete/:id",
+  passport.authenticate("bearer", { session: false }),
+  deleteBlog
+);
 
 export default router;
