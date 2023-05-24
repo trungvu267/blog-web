df62f087d012b63de7450dca1c5bc268d476885f
VuTrung <vutrung26072001@gmail.com>
2023-05-24 17:39:06 +0700

    format code

--- server/controllers/tag.controller.js
+++ server/controllers/tag.controller.js
@@ -1,119 +1,108 @@
 import Tag from "../models/tag.model.js";
 import { StatusCodes } from "http-status-codes";
 
-const getAllTags = async(req, res, next) => {
-    const tags = await Tag.find();
-    res.json(tags);
+const getAllTags = async (req, res, next) => {
+  const tags = await Tag.find();
+  res.json(tags);
 };
-const getOneTag = async(req, res, next) => {
-    res.json({ success: true, message: "find post success", tag: req.tag });
+const getOneTag = async (req, res, next) => {
+  res.json({ success: true, message: "find post success", tag: req.tag });
 };
 
-const createTag = async(req, res) => {
-    const { name, bg_color, text_color } = req.body;
-    if (!name) {
-        return res
-            .status(StatusCodes.BAD_REQUEST)
-            .json({ success: false, message: "tag name must be required" });
-    }
-    const newTag = new Tag({
-        name,
-        bg_color,
-        text_color,
+const createTag = async (req, res) => {
+  const { name, bg_color, text_color } = req.body;
+  if (!name) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: "tag name must be required" });
+  }
+  const newTag = new Tag({
+    name,
+    bg_color,
+    text_color,
+  });
+  await newTag.save();
 
-    })
-    await newTag.save();
+  res.json({
+    success: true,
+    message: "you have successfully created",
+    tag: newTag,
+  });
+};
 
-    res.json({
-        success: true,
-        message: "you have successfully created",
-        tag: newTag,
+const updateTag = async (req, res) => {
+  let updateTag = {
+    ...req.body,
+  };
+
+  const tagtUpdateCondition = {
+    _id: req.tag._id,
+  };
+  const tag = await Tag.findOne(tagtUpdateCondition);
+  if (!tag) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ success: false, message: "tag not found" });
+  }
+  if (req.user.role === "editor" && !!published) {
+    return res
+      .status(StatusCodes.NOT_MODIFIED)
+      .json({ success: false, message: "you are not Permissions" });
+  }
+  const newUpdateTag = await Tag.findOneAndUpdate(
+    tagtUpdateCondition,
+    updateTag,
+    { new: true }
+  );
+  // User not authorised to update post or post not found
+  if (!newUpdateTag)
+    return res.status(StatusCodes.FORBIDDEN).json({
+      success: false,
+      message: "tag not found or user not authorised",
     });
+
+  res.json({
+    success: true,
+    message: "updated tag was successfully updated",
+    tag: newUpdateTag,
+  });
 };
+const deleteTag = async (req, res) => {
+  const TagDeleteCondition = { _id: req.tag.id };
+  const tag = await Tag.findOne(TagDeleteCondition);
 
+  if (!tag) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ success: false, message: "tag not found" });
+  }
 
-const updateTag = async(req, res) => {
-    let updateTag = {
-        ...req.body,
-    };
+  const deletedTag = await Tag.findOneAndDelete(TagDeleteCondition);
 
-    const tagtUpdateCondition = {
-        _id: req.tag._id,
-
-    };
-    const tag = await Tag.findOne(tagtUpdateCondition);
-    if (!tag) {
-        return res
-            .status(StatusCodes.NOT_FOUND)
-            .json({ success: false, message: "tag not found" });
-    }
-    if (req.user.role === "editor" && !!published) {
-        return res
-            .status(StatusCodes.NOT_MODIFIED)
-            .json({ success: false, message: "you are not Permissions" });
-    }
-    const newUpdateTag = await Tag.findOneAndUpdate(
-        tagtUpdateCondition,
-        updateTag, { new: true }
-    );
-    // User not authorised to update post or post not found
-    if (!newUpdateTag)
-        return res.status(StatusCodes.FORBIDDEN).json({
-            success: false,
-            message: "tag not found or user not authorised",
-        });
-
-    res.json({
-        success: true,
-        message: "updated tag was successfully updated",
-        tag: newUpdateTag,
+  if (!deletedTag)
+    return res.status(StatusCodes.NOT_FOUND).json({
+      success: false,
+      message: "tag not found or user not authorised",
     });
-};
-const deleteTag = async(req, res) => {
-    const TagDeleteCondition = { _id: req.tag.id };
-    const tag = await Tag.findOne(TagDeleteCondition);
 
-    if (!tag) {
-        return res
-            .status(StatusCodes.NOT_FOUND)
-            .json({ success: false, message: "tag not found" });
-    }
-
-    const deletedTag = await Tag.findOneAndDelete(TagDeleteCondition);
-
-    if (!deletedTag)
-        return res.status(StatusCodes.NOT_FOUND).json({
-            success: false,
-            message: "tag not found or user not authorised",
-        });
-
-    res.json({
-        success: true,
-        message: "delete tag success",
-        tag: deletedTag,
-    });
+  res.json({
+    success: true,
+    message: "delete tag success",
+    tag: deletedTag,
+  });
 };
 
 const getTagId = (req, res, next, id) => {
-    Tag.findById(id)
-        .then((tag) => {
-            req.tag = tag;
-            next();
-        })
-        .catch((err) => {
-            console.log(err);
-            const notFound = new Error("Tag not found");
-            next(notFound);
-        });
+  Tag.findById(id)
+    .then((tag) => {
+      req.tag = tag;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+      const notFound = new Error("Tag not found");
+      next(notFound);
+    });
 };
 
-
-
-export {
-    getAllTags,
-    getOneTag,
-    createTag,
-    updateTag,
-    deleteTag,
-    getTagId,
-}
+export { getAllTags, getOneTag, createTag, updateTag, deleteTag, getTagId };
--- server/routes/tag.route.js
+++ server/routes/tag.route.js
@@ -2,26 +2,23 @@ import express from "express";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 
 import {
-    getAllTags,
-    getOneTag,
-    createTag,
-    updateTag,
-    deleteTag,
-    getTagId,
-} from "../controllers/tag.controller.js"
+  getAllTags,
+  getOneTag,
+  createTag,
+  updateTag,
+  deleteTag,
+  getTagId,
+} from "../controllers/tag.controller.js";
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-
-
-router.get('/getalltag', asyncWrapper(getAllTags));
-router.get('/:tagId/getonetag', asyncWrapper(getOneTag));
+router.get("/getalltag", asyncWrapper(getAllTags));
+router.get("/:tagId/getonetag", asyncWrapper(getOneTag));
 router.post("/", auth, asyncWrapper(createTag));
 router.put("/:tagId/updatetag", auth, asyncWrapper(updateTag));
 router.delete("/:tagId/delete", auth, asyncWrapper(deleteTag));
 
 router.param("tagId", getTagId);
-
 
 export default router;
