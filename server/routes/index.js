41f86e51f5de660b3f4ac07fc1681794e8653539
Truong2001nd <phamtruong14102001@gmail.com>
2023-05-24 17:31:40 +0700

    Crud tag

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,21 +3,23 @@ import passport from "passport";
 import userRouter from "./user.route.js";
 import blogsRouter from "./blogs.route.js";
 import commentRouter from "./comment.route.js";
+import tagRouter from "./tag.route.js";
 
 const Router = ExpressRouter();
 Router.get("/", (req, res) => {
-  res.json({ message: "Hello from Blog app 💓" });
+    res.json({ message: "Hello from Blog app 💓" });
 });
 // NOTE: test routers
 Router.get(
-  "/test",
-  passport.authenticate("bearer", { session: false }),
-  (req, res) => {
-    res.json(req.user);
-  }
+    "/test",
+    passport.authenticate("bearer", { session: false }),
+    (req, res) => {
+        res.json(req.user);
+    }
 );
 Router.use("/users", userRouter);
 Router.use("/blogs", blogsRouter);
 Router.use("/comments", commentRouter);
+Router.use("/tag", tagRouter);
 
 export default Router;
