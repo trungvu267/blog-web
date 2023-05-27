20c165d52d85909ec6cbeb6474c928385f347fe6
VuTrung <vutrung26072001@gmail.com>
2023-05-27 17:51:49 +0700

    add note

--- client/src/hooks/tag.hook.js
+++ client/src/hooks/tag.hook.js
@@ -3,6 +3,7 @@ import { listTagAtom } from "../states/tag.state";
 import { useQuery } from "react-query";
 import { getListTag } from "../api/tag.api";
 const useTag = () => {
+  // TODO: Add caching
   const [listTag, setListTag] = useAtom(listTagAtom);
   const { isLoading, error } = useQuery({
     queryKey: ["tags"],
