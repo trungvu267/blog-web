24465e3e79cc7dad8c57f3e5e8549482cd77968f
VuTrung <vutrung26072001@gmail.com>
2023-05-27 23:05:28 +0700

    add TagSkeleton

--- client/src/components/TagsPage/ListTag.jsx
+++ client/src/components/TagsPage/ListTag.jsx
@@ -1,8 +1,9 @@
 import Tag from "./Tag";
 import useTag from "../../hooks/tag.hook";
+import { TagSkeleton } from "../Skeleton";
 const ListTag = () => {
   const { listTag, isLoading } = useTag();
-  if (isLoading) return <div>...Loading</div>;
+  if (isLoading) return <ListTagSkeleton />;
   return (
     <div className="grid grid-cols-3 gap-10 mt-5">
       {listTag.map((tag) => (
@@ -13,3 +14,13 @@ const ListTag = () => {
 };
 
 export default ListTag;
+
+const ListTagSkeleton = () => {
+  return (
+    <div className="grid grid-cols-3 gap-10 mt-5">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <TagSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,4 @@ import ToastContainerCustomer from "./ToastContainerCustomer";
 import NavbarBlog from "./NavbarBlog";
 import Layout from "./Layout";
 import ConfirmModal from "./ConfirmModal";
-import Skeleton from "./Skeleton";
-export { ToastContainerCustomer, NavbarBlog, Layout, ConfirmModal, Skeleton };
+export { ToastContainerCustomer, NavbarBlog, Layout, ConfirmModal };
