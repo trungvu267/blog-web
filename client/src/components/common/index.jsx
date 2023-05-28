af182f9a9f5ac4128974dd162be70ace717a5896
VuTrung <vutrung26072001@gmail.com>
2023-05-28 23:53:13 +0700

    handle logic create post

--- client/src/components/common/index.jsx
+++ client/src/components/common/index.jsx
@@ -2,4 +2,5 @@ import Logo from "./Logo";
 import Avatar from "./Avatar";
 import RequestLoginModal from "./RequestLoginModal";
 import ToggleThemeBtn from "./ToggleThemeBtn";
-export { Logo, Avatar, RequestLoginModal, ToggleThemeBtn };
+import HtmlConverter from "./HtmlConverter";
+export { Logo, Avatar, RequestLoginModal, ToggleThemeBtn, HtmlConverter };
