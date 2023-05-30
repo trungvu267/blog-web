7325c6a84471c4e9a72650f7bbf2eb8fc05fa137
VuTrung <vutrung26072001@gmail.com>
2023-05-30 08:53:06 +0700

    update controller

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,14 +1,17 @@
 import express from "express";
 import asyncWrapper from "../middleware/asyncWrapper.js";
 import {
-    createComment,
-    updateComment,
-    deleteComment,
-    getCommentId,
+  createComment,
+  updateComment,
+  deleteComment,
+  getCommentId,
+  getListCommentByBlog,
 } from "../controllers/comment.controller.js";
 import { auth } from "../middleware/auth.js";
 const router = express.Router();
 
+router.get("/", asyncWrapper(getListCommentByBlog));
+router.post("/", auth, asyncWrapper(createComment));
 router.put("/:commentId", auth, asyncWrapper(updateComment));
 router.delete("/:commentId", auth, asyncWrapper(deleteComment));
 router.param("commentId", getCommentId);
