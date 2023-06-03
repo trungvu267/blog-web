ade40d543551bf8f216e1dc2afc75d0bf1a578dd
VuTrung <vutrung26072001@gmail.com>
2023-06-03 12:12:04 +0700

    update custom post hook

--- client/src/hooks/post.hook.js
+++ client/src/hooks/post.hook.js
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 import { listPostAtom } from "../states/post.state";
 import {
   getAllBlog,
@@ -8,27 +8,28 @@ import {
   updateBlog,
 } from "../api/post.api";
 import { successToast } from "../utils/toast";
+import { QUERY_KEYS } from "../utils/queryKeys";
 export const usePost = () => {
-  // TODO: Add caching
   const [listPost, setListPost] = useAtom(listPostAtom);
-  const { isLoading, error, refetch } = useQuery({
-    queryKey: ["blogs"],
+  const { isLoading, error } = useQuery({
+    queryKey: [QUERY_KEYS.posts],
     queryFn: getAllBlog,
     onSuccess: (res) => {
       setListPost(res.data);
     },
   });
-  return { isLoading, error, listPost, refetch };
+  return { isLoading, error, listPost };
 };
 
-export const useUpdatePost = (data, refetch) => {
+export const useUpdatePost = (data) => {
   const mutation = useMutation(updateBlog, {
     mutationKey: [`blogs/${data.blogId}`],
   });
+  const client = useQueryClient();
   const handleUpdateBlog = () => {
     mutation.mutate(data, {
       onSuccess: () => {
-        refetch();
+        client.invalidateQueries(QUERY_KEYS.posts);
         successToast("Cập nhật bài viết thành công");
       },
     });
--- client/src/utils/queryKeys.js
+++ client/src/utils/queryKeys.js
@@ -1,3 +1,4 @@
 export const QUERY_KEYS = {
   tags: "tags",
+  blogs: "blogs",
 };
