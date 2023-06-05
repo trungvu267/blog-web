c577b593879a4ce22dc23269035ea654ee168fd0
VuTrung <vutrung26072001@gmail.com>
2023-06-06 03:52:15 +0700

    create upload-image-module

--- client/src/states/textEditor.js
+++ client/src/states/textEditor.js
@@ -1,7 +1,9 @@
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 export const titleArticleAtom = atom("");
 export const contentArticleAtom = atom("");
 export const tagsArticleAtom = atom([]);
 export const toolTipEditorAtom = atom(null);
 export const isVisibleAtom = atom(false);
+export const titleImageLinkAtom = atomWithStorage("titleImageLink", "");
