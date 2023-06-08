f77c37cee7fa89aa08380c0ef077bd648c8f356e
VuTrung <vutrung26072001@gmail.com>
2023-06-09 00:26:01 +0700

    add config file

---
+++ server/config/config.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+export default {
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    timeLife: process.env.JWT_TIME_LIFE,
+  },
+  mongo: {
+    uri: process.env.MONGO_URI,
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    bucketName: process.env.AWS_BUCKET_NAME,
+    region: process.env.AWS_REGION,
+  },
+  port: process.env.PORT,
+  client: {
+    url: process.env.CLIENT_URL,
+  },
+};
