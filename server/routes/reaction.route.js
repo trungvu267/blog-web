93c62354e7e342c04eed57d6631b843add555a58
VuTrung <vutrung26072001@gmail.com>
2023-06-10 22:57:40 +0700

    update reaction module

--- server/routes/reaction.route.js
+++ server/routes/reaction.route.js
@@ -1,15 +1,10 @@
 import express from "express";
 import asyncWrapper from "../middleware/asyncWrapper.js";
-
-import {
-  likeBlog,
-  getListLikedByUser,
-} from "../controllers/like.controller.js";
+import reaction from "../controllers/reaction.controller.js";
 
 import { auth } from "../middleware/auth.js";
 const router = express.Router();
 
-router.get("/", auth, asyncWrapper(getListLikedByUser));
-router.post("/", auth, asyncWrapper(likeBlog));
+router.get("/", auth, asyncWrapper(reaction.getListReactionByUser));
 
 export default router;
